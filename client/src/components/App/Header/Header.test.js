19e082d1aa58b31291ffe456dba2f0c562f512d1
Myron Mctyer <myronsdev@gmail.com>
2019-05-06 21:24:04 -0500

    add test for home and header components

---
+++ client/src/components/App/Header/Header.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './Header'
+import { BrowserRouter as Router } from 'react-router-dom'
+import renderer from 'react-test-renderer'
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(
+      <Router>
+        <Header />
+      </Router>
+    )
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+console.log(Header)
