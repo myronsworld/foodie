cdf79c45a5fab11a900c52e9e9d6eb7937e4d17c
Myron Mctyer <myronsdev@gmail.com>
2019-05-08 01:47:24 -0500

    Render recipe to form -- to allow for editing

--- client/src/components/App/Pages/Profile/Recipe/Recipe.js
+++ client/src/components/App/Pages/Profile/Recipe/Recipe.js
@@ -6,8 +6,24 @@ class Recipe extends Component {
     super(props)
 
     this.state = {
-      recipe: {}
+      title: '',
+      cookTime: 0,
+      description: '',
+      directions: [],
+      foodType: {},
+      ingredients: {},
+      prepTime: 0,
+      serves: 0
     }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    const target = event.target
+    const value = target.value
+    const name = target.name
+
+    this.setState({ [name]: value })
   }
 
   async componentDidMount() {
@@ -15,30 +31,126 @@ class Recipe extends Component {
     const id = splitURL[splitURL.length - 1]
 
     try {
-      const res = await axios.get(`/api/recipes/${id}`)
+      const res = await axios.get(`/api/recipes/${id}`, {
+        headers: {
+          'Content-Type': 'applicatdion/json'
+        }
+      })
 
       if (res) {
-        console.log(res.data)
-        this.setState(() => ({ recipe: res.data }))
+        delete res.data.chef
+        delete res.data.__v
+        delete res.data._id
+        delete res.data.createdAt
+        delete res.data.rating
+        delete res.data.updatedAt
+
+        this.setState(() => res.data)
+        console.log(this.state)
       }
     } catch (e) {
       console.log(e)
     }
   }
 
-  renderRecipe() {
-    const { title, cookTime, description, directions, foodType, ingredients, prepTime, serves } = this.state.recipe
+  /// need to add handle this these and setstate for each piece of state
 
+  renderInputs(arr) {
+    // arr.map((item) => {
+    //   return (
+    //     <div className="control">
+    //       <input name="cookTime" onChange={this.handleChange} className="input is-primary" type="number" value={item} />
+    //     </div>
+    //   )
+    // })
+
+    return <div>df</div>
+  }
+
+  renderRecipe() {
+    const { title, cookTime, description, directions, foodType, ingredients, prepTime, serves } = this.state
     return (
       <div className="content">
-        <h1>{title}</h1>
-        <p>{description}</p>
-        <p>cooktime: {cookTime}</p>
-        <p>preptime: {prepTime}</p>
-        <p>foodType: {foodType}</p>
-        <p>serves: {serves}</p>
-        <p>{ingredients}</p>
-        <p>{directions}</p>
+        <form>
+          <div className="field">
+            <label className="label">Title</label>
+            <div className="control">
+              <input onChange={this.handleChange} name="title" className="input is-primary" type="text" value={title} />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Description</label>
+            <div className="control">
+              <input
+                onChange={this.handleChange}
+                name="description"
+                className="input is-primary"
+                type="text"
+                value={description}
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Cooktime</label>
+            <div className="control">
+              <input
+                name="cookTime"
+                onChange={this.handleChange}
+                className="input is-primary"
+                type="number"
+                value={cookTime}
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Preptime</label>
+            <div className="control">
+              <input
+                name="prepTime"
+                onChange={this.handleChange}
+                className="input is-primary"
+                type="number"
+                value={prepTime}
+              />
+            </div>
+          </div>
+          {/* <div className="field">
+            <label className="label">FoodType</label>
+            {this.renderInputs()}
+          </div> */}
+          <div className="field">
+            <label className="label">Serves</label>
+            <div className="control">
+              <input
+                name="serves"
+                onChange={this.handleChange}
+                className="input is-primary"
+                type="number"
+                value={serves}
+              />
+            </div>
+          </div>
+          {/* <div className="field">
+            <label className="label">Ingredients</label>
+            {this.renderInputs(ingredients)}
+          </div> */}
+          <div className="field">
+            <label className="label">Directions </label>
+            <div className="control">
+              <input
+                name="directions"
+                onChange={this.handleChange}
+                className="input is-primary"
+                type="text"
+                value={directions}
+              />
+            </div>
+          </div>
+        </form>
+
+        <button href="" className="button is-link">
+          Save
+        </button>
       </div>
     )
   }
