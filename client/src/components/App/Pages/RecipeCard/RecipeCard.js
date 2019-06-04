0bc888626ac7fb97e8c46f205f6ed98878cbdeed
Myron Mctyer <myronsdev@gmail.com>
2019-06-03 23:20:22 -0500

    Update homepage recipe styles

--- client/src/components/App/Pages/RecipeCard/RecipeCard.js
+++ client/src/components/App/Pages/RecipeCard/RecipeCard.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import styles from './styles.module.css'
 
 class RecipeCard extends Component {
   render() {
     const { _id, title, description, serves, prepTime, cookTime } = this.props.recipe
     return (
-      <div key={_id} className={styles.cardStyle + ` card`}>
+      <div key={_id} className={styles.cardStyle}>
         <header className="card-header">
-          <p className={styles['title-style'] + ` card-header-title`}>{title}</p>
+          <p className={styles['title-style'] + ` card-header-title`}>
+            <Link to={`recipes/` + _id}>{title}</Link>
+          </p>
         </header>
         <div className="card-content">
           <p className={styles.paragraphs}>{description}</p>
         </div>
         <footer className="card-footer">
-          <div className="card-footer-item">serves: {serves}</div>
-          <div className="card-footer-item">preptime: {prepTime}</div>
-          <div className="card-footer-item">cooktime: {cookTime}</div>
+          <div className={styles['card-footer-item-style'] + ` card-footer-item`}>serves: {serves}</div>
+          <div className={styles['card-footer-item-style'] + ` card-footer-item`}>preptime: {prepTime}</div>
+          <div className={styles['card-footer-item-style'] + ` card-footer-item`}>cooktime: {cookTime}</div>
         </footer>
       </div>
     )
